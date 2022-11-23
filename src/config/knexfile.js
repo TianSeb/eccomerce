3332e4f8dfb5b3e5ca97b963197c2a13791af1b0
TianSeb <sebastianalasia@hotmail.com>
2022-11-23 20:47:16 -0300

    mongo

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
     development: {
         client: 'pg',
-        connection: 'postgres://user:pass@localhost:5432/eccomerce',
+        connection: 'mongodb://user:pass@mongo:27017/coderhouse',
         searchPath:  ['knex', 'public'],
         pool: { min: 0, max: 7 },
     },
     production: {
         client: 'pg',
-        connection: 'postgres://user:pass@localhost:5432/eccomerce',
+        connection: 'mongodb://user:pass@mongo:27017/coderhouse',
         searchPath:  ['knex', 'public'],
         pool: { min: 0, max: 7 },
     },
